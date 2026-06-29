c4f0da684290e185d100a92dbc1bc2779ff18235
Panthagi Praharshitha <221fa04075@gmail.com>
2026-06-29 22:52:15 +0530

    Day 4 done

---
+++ DAY-4(DSA)/136_Single_Number.py
@@ -0,0 +1,8 @@
+from typing import List
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+
+        for n in nums:
+            if nums.count(n) == 1:
+                return n
