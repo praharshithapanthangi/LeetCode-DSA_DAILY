5b8e27f5b7dce7e1fa3f792791dc5ff063f26f75
Panthagi Praharshitha <221fa04075@gmail.com>
2026-06-30 22:48:08 +0530

    Day 5 Done

---
+++ DAY-5(DSA)/283_Move_Zeroes.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+
+        n = len(nums)
+
+        for i in range(n):
+
+            if nums[i] == 0:
+
+                for j in range(i + 1, n):
+
+                    if nums[j] != 0:
+
+                        nums[i], nums[j] = nums[j], nums[i]
